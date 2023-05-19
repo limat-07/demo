fbf940c01d40152444cc57d1dff6be6bf031dfe3
limat-07 <whistler.20463982@gmail.com>
2023-05-19 09:43:50 +0530

    modified

--- src/javacode/c3.java
+++ src/javacode/c3.java
@@ -1,5 +1,9 @@
 package javacode;
 
 public class c3 {
-
+public static void main(String[] args) {
+	System.out.println("enter successful");
+	System.out.println("enter successful");
+	System.out.println("enter successful");
+}
 }
